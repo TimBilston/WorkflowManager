daaa3f5ac2e35600436aafedea2cb9934457bd5a
david <dade0001@student.monash.edu>
2021-09-09 05:08:19 +1000

    title size and button placement

--- templates/Pages/home.php
+++ templates/Pages/home.php
@@ -158,8 +158,8 @@ if ($this->Identity->isLoggedIn()) {
                 '<p class="person">'.$clientName.'</p>'.
                 '<p class="employee">'.$task->user->name.'</p>'.
                 '<p class="status">'.$task->status->name.'</p>'.
-                '<p class="button" style="padding: 1px;">'.$this->Html->link(__('View'), ['controller' => 'tasks', 'action' => 'view', $task->id]).' </p>'.
-                '<p class="button" style="padding: 1px; text-align:left">'.$this->Html->link(__('Complete'), ['controller' => 'tasks', 'action' => 'completeTask', $task->id]).'</p>'.
+                '<p class="button" style="padding: 1px; text-align:center">'.$this->Html->link(__('View'), ['controller' => 'tasks', 'action' => 'view', $task->id]).' </p>'.
+                '<p class="button" style="padding: 1px; text-align:center">'.$this->Html->link(__('Complete'), ['controller' => 'tasks', 'action' => 'completeTask', $task->id]).'</p>'.
                 '</li>';
         } ?>
 
