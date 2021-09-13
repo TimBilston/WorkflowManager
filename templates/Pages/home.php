fce1a16fee0a796581508a83ff86b25fe240cfe7
david <dade0001@student.monash.edu>
2021-09-13 18:59:56 +1000

    icon move

--- templates/Pages/home.php
+++ templates/Pages/home.php
@@ -427,7 +427,7 @@ if (!empty($task->subtasks)) {
     </div>
     <?php include('navigation.php') ?>
     <footer class="w3-container w3-padding-64 w3-center w3-opacity w3-xlarge" style="margin-top:20px; padding-left: 220px; background: #ffebeb; ">
-        <b style="color:#000000"><i class="fa fa-table"></i>This Week Total: <span id="tasksTotal" style="color:#b80c3c;"> 0 </span> tasks</b>
+        <b style="color:#000000"><i class="fa fa-table"></i> This Week Total: <span id="tasksTotal" style="color:#b80c3c;"> 0 </span> tasks</b>
     </footer>
 </html>
 <script type = "text/javascript" src = "js/jquery-1.4.1.js" ></script>
